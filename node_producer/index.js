3d8ab0eba2eac7205689dffa66adb297d1c093b2
Aditya Takharya <adityatakharya@gmail.com>
2025-07-21 03:26:39 +0530

    Wrapped the node_producer in express for better deployment

--- node_producer/index.js
+++ node_producer/index.js
@@ -1,9 +1,19 @@
 const {producer} = require('./services/kafka')
 const {generateLogEvent} = require('./utils/generateEvent')
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => res.send("Kafka Producer is running"));
+app.get("/healthz", (req, res) => res.status(200).send("OK"));
+
+app.listen(PORT, () => {
+  console.log(`Dummy HTTP server running on port ${PORT}`);
+});
 
 const run = async () => {
     console.log("BROKE URL", process.env.KAFKA_BROKER_URL)
-    
+
     await producer.connect();
 
     setInterval(async () => {
